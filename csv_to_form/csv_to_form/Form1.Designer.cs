59be105dd58959bb0efe86a789f72b1438b7bdbd
em-yabloko <57377898+em-yabloko@users.noreply.github.com>
2022-10-14 02:16:02 +0700

    Add files via upload

---
+++ csv_to_form/csv_to_form/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace csv_to_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Area = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(738, 575);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(492, 98);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Code,
+            this.Name1,
+            this.Area});
+            this.dataGridView1.Location = new System.Drawing.Point(30, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1200, 468);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.MinimumWidth = 8;
+            this.id.Name = "id";
+            this.id.Width = 40;
+            // 
+            // Code
+            // 
+            this.Code.HeaderText = "Код";
+            this.Code.MinimumWidth = 8;
+            this.Code.Name = "Code";
+            this.Code.Width = 120;
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "Наименование";
+            this.Name1.MinimumWidth = 8;
+            this.Name1.Name = "Name1";
+            this.Name1.Width = 454;
+            // 
+            // Area
+            // 
+            this.Area.HeaderText = "Подразделение";
+            this.Area.MinimumWidth = 8;
+            this.Area.Name = "Area";
+            this.Area.Width = 122;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(87, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Receive DB from CSV";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 685);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Area;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ csv_to_form/csv_to_form/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace csv_to_form
+{
+        public partial class Form1 : Form
+    {
+        // txt посредник
+        string addr2 = @"C:\temp_csv\test.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection conn = null;
+        private string source_csv;
+
+        public string open_file() // диалог выбора файла
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choose your CSV";
+            ofd.InitialDirectory = @"c:\";
+            ofd.Filter = "All files (*.*)|*.csv|All files (*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                source_csv = ofd.FileName;
+            }
+            return source_csv;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            string new_addr1 = open_file(); // адрес открытого
+            if (new_addr1 != null)          // если адрес не отменен
+            {
+                // перекодировка в кириллицу
+                using (StreamReader sr = new StreamReader(new_addr1, Encoding.Default))
+                {
+                    // сброс в нужной кодировке посреднику
+                    using (FileStream fs = File.Create(addr2))
+                    {
+                        while (sr.EndOfStream == false)
+                        {
+                            string data = sr.ReadLine();
+                            byte[] info = new UTF8Encoding(true).GetBytes(data + Environment.NewLine);
+                            fs.Write(info, 0, info.Length);
+                        }
+                    }
+                }
+                cll();  // разбивка на кадры из txt и склад в sql
+
+                // из sql в таблицу Form1
+                string query = "SELECT * FROM [Table] ORDER BY Id";
+                SqlCommand cmd_grid = new SqlCommand(query, conn);
+                SqlDataReader dr = cmd_grid.ExecuteReader();
+                List<string[]> list = new List<string[]>();
+                while (dr.Read())
+                {
+                    list.Add(new string[4]);
+
+                    list[list.Count - 1][0] = dr[0].ToString();
+                    list[list.Count - 1][1] = dr[1].ToString();
+                    list[list.Count - 1][2] = dr[2].ToString();
+                    list[list.Count - 1][3] = dr[3].ToString();
+                }
+                dr.Close();
+
+                dataGridView1.Rows.Clear();
+                foreach (string[] list2 in list)
+                    dataGridView1.Rows.Add(list2);
+            }
+        }
+
+        void cll()
+        {
+            using (TextFieldParser res = new TextFieldParser(addr2))
+            {
+                SqlCommand cmd_clr = new SqlCommand($"TRUNCATE TABLE [Table]", conn);
+                cmd_clr.ExecuteNonQuery();
+
+                res.TextFieldType = FieldType.Delimited;
+                res.SetDelimiters(";");
+
+                while (!res.EndOfData)
+                {
+                    string[] datas = res.ReadFields();
+
+                    SqlCommand cmd_add = new SqlCommand($"INSERT INTO [Table] (Code, Name, Area) VALUES (N'{datas[0]}',N'{datas[1]}',N'{datas[2]}')", conn);
+                    cmd_add.ExecuteNonQuery();
+                }
+            }
+        }
+
+         private void Form1_Load(object sender, EventArgs e)
+        {
+            // коннект к sql
+            string way = Application.StartupPath; // текущая директория
+            string addr_conn = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = "+way+"\\Database1.mdf;Integrated Security=True";
+            conn = new SqlConnection(addr_conn);
+
+            conn.Open();
+
+            // папка для посредника
+            DirectoryInfo di = new DirectoryInfo(@"c:\temp_csv");
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+         }
+    }
+}
